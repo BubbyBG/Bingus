8f32717d8927e1460b012664b80e77d98c9ae0fe
mjohn132 <matthew.john@wsu.edu>
2025-01-27 19:38:30 -0800

    Add NPC/Enemy Classes (outlines)

---
+++ CS320Project/Assets/EnemyClass.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+/*  This will contain traits and behaviors more specific to
+    enemies. This class inherits traits from NPCClass.
+*/
+public class EnemyClass : NPC
+{
+    
+    //Fields...
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        //Start code here
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Update code here
+    }
+}
