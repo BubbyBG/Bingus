0527a5390ffaa045ac6c50e2df09dd2ef3297b39
JordanW-Projects <jordanwatts085@gmail.com>
2025-03-21 22:52:47 -0700

    modified how music is played, now music player works

--- CS320Project/Assets/Items/MusicItem.cs
+++ CS320Project/Assets/Items/MusicItem.cs
@@ -4,22 +4,15 @@ public class MusicItem : ItemClass //inherits from ItemClass
 {
 
     private SanityState sanityState;
-
+    
     void Start()
     {
+       
         sanityState = FindAnyObjectByType<SanityState>(); //there will only be one sanity state so this shouldn't find anything else
+        
     }
 
-    void Update()
-    {   
-        //print("workjing");
-        if (held && Input.GetKeyDown("f")) {
-            print("starting sanity gain");
-            sanityState.startSanityGain();
-        }
-        if(!held) {
-            print("starting sanity loss");
-            sanityState.startSanityLoss();
-        }
-    }
+    
+
+   
 }
